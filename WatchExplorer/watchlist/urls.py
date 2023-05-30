bd8b6f67eaa072fc9d794519beee00d7accb4894
serg <feelwork2077@gmail.com>
2023-05-30 11:55:10 +0200

    fixed bugs with urls

--- WatchExplorer/watchlist/urls.py
+++ WatchExplorer/watchlist/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path, include
-from django.contrib import admin
-
+from django.urls import path
+from watchlist.views import home, watch_list, search_watch_data
+app_name = 'watchlist'
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('watchlist.urls')),
+    path('', watch_list, name='watch_list'),
+    path('search/', search_watch_data, name='search'),
 ]
--- WatchExplorer/watchlist/views.py
+++ WatchExplorer/watchlist/views.py
@@ -1,19 +1,22 @@
 from django.shortcuts import render
 from .models import WatchData
 
-def home(request):
+
+def home(request):  # http://127.0.0.1:8000/
     saved_watch_data = WatchData.objects.all()
     context = {
         'saved_watch_data': saved_watch_data
     }
     return render(request, 'watchlist/home.html', context)
 
-def search_watch_data(request):
+
+def search_watch_data(request):     # http://127.0.0.1:8000/watchlist/search/
     return render(request, 'watchlist/search.html')
 
-def watch_list(request):
+
+def watch_list(request):    # http://127.0.0.1:8000/watchlist/
     saved_watch_data = WatchData.objects.all()
     context = {
         'saved_watch_data': saved_watch_data
     }
-    return render(request, 'watch-list.html', context)
+    return render(request, 'watchlist/watch-list.html', context)
